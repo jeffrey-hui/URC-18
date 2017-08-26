e0d11399409422eb8161d76b6cc6a02d317393cf
mincrmatt12 <matthewmirvish@hotmail.com>
2017-08-25 22:25:20 -0400

    servo test

---
+++ rosws/src/rover_drive/src/simple_drive_node/constants.h
@@ -0,0 +1,21 @@
+//
+// Created by matthew on 22/08/17.
+//
+
+#ifndef PROJECT_CONSTANTS_H
+#define PROJECT_CONSTANTS_H
+
+#include <cstdint>
+#include <array>
+
+namespace rover_drive {
+
+    const std::array<uint8_t, 1> LEFT_WHEELS = {9}; // expandable for different numbers of wheels
+    const std::array<uint8_t, 1> RIGHT_WHEELS = {10};
+
+    const int MOTOR_MID = 1500;
+    const int MOTOR_OFFSET = 500;
+
+}
+
+#endif //PROJECT_CONSTANTS_H
