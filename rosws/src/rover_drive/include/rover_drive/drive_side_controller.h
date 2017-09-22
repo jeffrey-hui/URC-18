4acb20db7e24cb81d51a5fcdbc5441d2e98d34e4
mincrmatt12 <matthewmirvish@hotmail.com>
2017-09-21 20:16:33 -0400

    sync

--- rosws/src/rover_drive/include/rover_drive/drive_side_controller.h
+++ rosws/src/rover_drive/include/rover_drive/drive_side_controller.h
@@ -7,10 +7,6 @@
 
 #include <hardware_interface/joint_command_interface.h>
 #include <controller_interface/controller.h>
-#include <ros/ros.h>
-#include <std_msgs/Float64.h>
-#include <realtime_tools/realtime_buffer.h>
-#include <pluginlib/class_list_macros.h>
 
 namespace rover_drive {
 
@@ -21,22 +17,11 @@ namespace rover_drive {
         }
 
         ~DriveSideController() {
-            sub.shutdown();
+
         }
 
-        virtual bool init(hardware_interface::VelocityJointInterface *hw, ros::NodeHandle &nh);
-        void update(const ros::Time& time, const ros::Duration& period);
-
-        void velCB(const std_msgs::Float64 &p);
-
-    private:
-
-        ros::Subscriber sub;
-        std::vector<hardware_interface::JointHandle> handle;
-        realtime_tools::RealtimeBuffer<double> dat;
 
     };
-
 
 }
 #endif //PROJECT_DRIVE_SIDE_CONTROLLER_H
