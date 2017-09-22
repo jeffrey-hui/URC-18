cf7f9d1e0495737a5c625ad1239522eb78e7b228
mincrmatt12 <matthewmirvish@hotmail.com>
2017-09-22 16:58:39 -0400

    yay

--- rosws/src/rover_drive/src/drive_hw/hw_impl.cpp
+++ rosws/src/rover_drive/src/drive_hw/hw_impl.cpp
@@ -6,31 +6,35 @@
 #include <hardware_interface/joint_command_interface.h>
 #include "ard_device.h"
 #include "constants.h"
-#include <rover_drive/hw_impl.h>
 
 namespace rover_drive {
+
+    class DriveHW {
+    public:
+        DriveHW() : device(1, 0x30);
+        
+        void init(hardware_interface::RobotHW *hw);
+
+        void read();
+
+        void write();
+
+    private:
+        ARDevice device;
+        double cmd[6];
+
+        hardware_interface::JointStateInterface jnt_state_interface;
+        hardware_interface::VelocityJointInterface jnt_vel_interface;
+
+    };
     
     uint16_t convert_to_msecs(double vel) {
         double clamped = std::min(5.0d, std::max(-5.0d, vel));
         double v = clamped / 5.0;
-        return (uint16_t)(((MOTOR_OFFSET * v)) + MOTOR_MID);
+        return ((uint16_t)(MOTOR_OFFSET * v)) + MOTOR_MID;
     }
 
     void DriveHW::init(hardware_interface::RobotHW *hw) {
-        hardware_interface::JointStateHandle jsLB("leg_to_wheel_back_left", &pos[0], &vel[0], &eff[0]);
-        hardware_interface::JointStateHandle jsRB("leg_to_wheel_back_right", &pos[3], &vel[3], &eff[3]);
-        hardware_interface::JointStateHandle jsLF("leg_to_wheel_front_left", &pos[1], &vel[1], &eff[1]);
-        hardware_interface::JointStateHandle jsRF("leg_to_wheel_front_right", &pos[4], &vel[4], &eff[4]);
-        hardware_interface::JointStateHandle jsLM("leg_to_wheel_center_left", &pos[2], &vel[2], &eff[2]);
-        hardware_interface::JointStateHandle jsRM("leg_to_wheel_center_right", &pos[5], &vel[5], &eff[5]);
-
-        jnt_state_interface.registerHandle(jsLB);
-        jnt_state_interface.registerHandle(jsLF);
-        jnt_state_interface.registerHandle(jsLM);
-        jnt_state_interface.registerHandle(jsRB);
-        jnt_state_interface.registerHandle(jsRF);
-        jnt_state_interface.registerHandle(jsRM);
-        
         hw->registerInterface(&jnt_state_interface);
         hardware_interface::JointHandle jLB(jnt_state_interface.getHandle("leg_to_wheel_back_left"), &cmd[0]);
         hardware_interface::JointHandle jRB(jnt_state_interface.getHandle("leg_to_wheel_back_right"), &cmd[3]);
--- rosws/src/rover_teleoperation/src/simple_drive_joy_node.py
+++ rosws/src/rover_teleoperation/src/simple_drive_joy_node.py
@@ -14,7 +14,7 @@ drive_control_right = rospy.Publisher("/right_wheels_controller/cmd", std_msgs.m
 
 
 def ctrl_curve(val):
-    return math.copysign(val, val) *1.75
+    return math.copysign(val ** 2, val)
 
 
 def on_joy_data(msg):
@@ -22,8 +22,8 @@ def on_joy_data(msg):
 
     left = ctrl_curve(msg.axes[LEFT_AXIS])
     right = ctrl_curve(msg.axes[RIGHT_AXIS])
-    drive_control_right.publish(left)
-    drive_control_left.publish(-right)
+    drive_control_left.publish(left)
+    drive_control_right.publish(right)
 
 joy_listener = rospy.Subscriber("joy", sensor_msgs.msg.Joy, queue_size=20, callback=on_joy_data)
 rospy.spin()
