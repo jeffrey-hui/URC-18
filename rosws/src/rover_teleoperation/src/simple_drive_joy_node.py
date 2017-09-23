bef56a46530abcaebce09a1186f8d2380e0aeb42
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2017-09-22 20:38:19 -0400

    Simple drive -- no ros_control (#1)
    Merging now due to being superseded by ros_control, this was tested on hardware (and no one reviewed it so I have to use "administrator privileges" to merge it :) )

---
+++ rosws/src/rover_teleoperation/src/simple_drive_joy_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import math
+import std_msgs.msg
+import sensor_msgs.msg
+import rospy
+
+LEFT_AXIS = 1
+RIGHT_AXIS = 4
+
+rospy.loginfo("Starting drive joy teleoperation node")
+rospy.init_node("simple_drive_joy_node")
+drive_control_left = rospy.Publisher("/drive/left", std_msgs.msg.Float32, queue_size=20)
+drive_control_right = rospy.Publisher("/drive/right", std_msgs.msg.Float32, queue_size=20)
+
+
+def ctrl_curve(val):
+    return math.copysign(val, val ** 2)
+
+
+def on_joy_data(msg):
+    # type: (sensor_msgs.msg.Joy) -> None
+
+    left = ctrl_curve(msg.axes[LEFT_AXIS])
+    right = ctrl_curve(msg.axes[RIGHT_AXIS])
+    drive_control_left.publish(left)
+    drive_control_right.publish(right)
+
+joy_listener = rospy.Subscriber("joy", sensor_msgs.msg.Joy, queue_size=20, callback=on_joy_data)
+rospy.spin()
