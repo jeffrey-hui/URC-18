66fe4f5f20cb3a0b8893b5b0d8f66c3015064673
mincrmatt12 <matthewmirvish@hotmail.com>
2017-11-08 19:19:40 -0500

    fix constants

--- rosws/src/rover_drive/src/drive_hw/constants.h
+++ rosws/src/rover_drive/src/drive_hw/constants.h
@@ -10,13 +10,13 @@
 
 namespace rover_drive {
 
-    const int LEFT_BACK_WHEEL = 7;
-    const int LEFT_FRONT_WHEEL = 8;
-    const int LEFT_MID_WHEEL = 9;
+    const int LEFT_BACK_WHEEL = 13;
+    const int LEFT_FRONT_WHEEL = 12;
+    const int LEFT_MID_WHEEL = 11;
 
-    const int RIGHT_BACK_WHEEL = 11;
-    const int RIGHT_FRONT_WHEEL = 12;
-    const int RIGHT_MID_WHEEL = 10;
+    const int RIGHT_BACK_WHEEL = 10;
+    const int RIGHT_FRONT_WHEEL = 9;
+    const int RIGHT_MID_WHEEL = 8;
 
     const int MOTOR_MID    = 1500;
     const int MOTOR_OFFSET = 700;
