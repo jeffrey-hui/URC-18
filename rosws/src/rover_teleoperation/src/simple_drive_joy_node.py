faea385956a976259f43326e190acb94ce5d5bc3
mincrmatt12 <matthewmirvish@hotmail.com>
2017-11-23 15:19:37 -0500

    merge in new teleop
    (cherry picked from commit ab3c0ac)

--- rosws/src/rover_teleoperation/src/simple_drive_joy_node.py
+++ rosws/src/rover_teleoperation/src/simple_drive_joy_node.py
@@ -12,18 +12,35 @@ rospy.init_node("simple_drive_joy_node")
 drive_control_left = rospy.Publisher("/left_wheels_controller/cmd", std_msgs.msg.Float64, queue_size=20)
 drive_control_right = rospy.Publisher("/right_wheels_controller/cmd", std_msgs.msg.Float64, queue_size=20)
 
+speed_value = 1.175
+
 
 def ctrl_curve(val):
-    return math.copysign(val ** 2, val)
+    return math.copysign(val ** 3, val) * speed_value
+
+
+push_button_last = False
 
 
 def on_joy_data(msg):
+    global push_button_last
+    global speed_value
     # type: (sensor_msgs.msg.Joy) -> None
 
     left = ctrl_curve(msg.axes[LEFT_AXIS])
     right = ctrl_curve(msg.axes[RIGHT_AXIS])
-    drive_control_left.publish(left)
-    drive_control_right.publish(-right)
+
+    tri_state = int(msg.axes[7])
+    if tri_state == 0:
+        push_button_last = False
+    else:
+        if not push_button_last:
+            speed_value += tri_state
+            speed_value = max(1.175, min(4.75, speed_value))
+
+    drive_control_right.publish(left)
+    drive_control_left.publish(-right)
+
 
 joy_listener = rospy.Subscriber("joy", sensor_msgs.msg.Joy, queue_size=20, callback=on_joy_data)
 rospy.spin()
