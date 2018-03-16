de7720ab45d02728b75ec70b851251ba3b16a670
mincrmatt12 <matthewmirvish@hotmail.com>
2018-03-16 16:09:53 -0400

    add tennis ball predictor

---
+++ rosws/src/rover_tennis_balls/src/tennis_ball_predictor/collider.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy.optimize import least_squares
+
+
+class Ray:
+    def __init__(self, pos, vec, conf):
+        self.pos = np.array(pos)
+        self.vec = np.array(vec)
+        self.conf = conf
+
+
+class Point:
+    def __init__(self, pos, conf):
+        self.pos = np.array(pos)
+        self.conf = conf
+
+
+def f(pt, rays, points):
+    errors = np.full([(len(rays) + len(points)) * 3], np.inf)
+    for i in range(0, len(rays)):
+        ray = rays[i]  # type: Ray
+        time_on_ray = ray.vec.dot((pt - ray.pos).T/ray.vec.dot(ray.vec.T))
+        if time_on_ray > 0:
+            errors[i*3:(i+1)*3] = (pt - (ray.pos + time_on_ray * ray.vec)).T * ray.conf
+        else:
+            errors[i * 3:(i + 1) * 3] = (pt - ray.pos).T * ray.conf
+    k = len(rays) * 3
+    for i in range(0, len(points)):
+        point = points[i]
+        errors[k + i * 3:k + (i+1) * 3] = (pt - point.pos) * point.conf
+
+    return errors.ravel()
+
+
+def calc_min_f(rays, points):
+    rays_p = [ray.pos for ray in rays] + [point.pos for point in points]
+    guess = np.stack(rays_p).mean(axis=0).ravel()
+
+    ans = least_squares(f, guess, args=(rays, points))
+
+    return ans
---
+++ rosws/src/rover_tennis_balls/src/tennis_ball_predictor/tfman.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+import rospy
+import geometry_msgs.msg
+import rover_tennis_balls.msg
+import tf2_ros
+
+import collider
+
+
+class TfMan:
+    def __init__(self, fixed):
+        self.buf = tf2_ros.Buffer()
+        self.buf_interface = tf2_ros.BufferInterface()
+        self.listener = tf2_ros.TransformListener(self.buf)
+        self.fixed = fixed
+
+    def get_collider_object(self, msg):
+        """
+        converts msg to an object from collider.py
+
+        :type msg: rover_tennis_balls.msg.TennisBall
+        """
+
+        if msg.tennis_ball_type == msg.TENNIS_BALL_RAY:
+            origin = tf2_ros.Stamped(geometry_msgs.msg.Vector3(
+                0, 0, 0
+            ), msg.header.stamp, msg.header.frame_id)
+            origin = self.buf_interface.transform(
+                origin, self.fixed, rospy.Duration.from_sec(0.2)
+            )
+            pos = tf2_ros.Stamped(msg.position_or_ray_dir, msg.header.stamp, msg.header.frame_id)
+            pos = self.buf_interface.transform(
+               pos, self.fixed, rospy.Duration.from_sec(0.2)
+            )
+            origin = np.array([
+                origin.x, origin.y, origin.z
+            ])
+            pos = np.array([
+                pos.x, pos.y, pos.z
+            ])
+            pos -= origin
+            return collider.Ray(origin, pos, msg.confidence)
+        else:
+            pos = tf2_ros.Stamped(msg.position_or_ray_dir, msg.header.stamp, msg.header.frame_id)
+            pos = self.buf_interface.transform(
+                pos, self.fixed, rospy.Duration.from_sec(0.2)
+            )
+            return collider.Point(pos, msg.confidence)
