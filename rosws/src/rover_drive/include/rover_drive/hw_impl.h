8e9531296cb0d92697c3bc192171b80bdef23f52
Matthew Mirvish <mincrmatt12@users.noreply.github.com>
2018-04-09 21:36:32 -0400

    Uberfirmware split (#13)
    * move eml_uberdriver
    
    * almost done docs for eml_uberdriver
    
    WARNING: this will probably get moved out to rtd for a seperate system, but for now
    it is staying here
    
    * fix uberfirmware
    
    * update docs for arduino protocol to be pointing to eml_uberdriver
    
    * yes
    
    * add controller_diagnostics to wstool
    
    * start work on diagnostics
    
    needs:
    
    - diagnostics to be implemented (waiting on eml to merge)
    
    * 51 dollars for a new calibration...
    
    MONEY WELL SPENT!!!!
    
    (cherry picked from commit a9eb967)
    
    * yes
    
    * add controller_diagnostics to wstool
    
    * start work on diagnostics
    
    needs:
    
    - diagnostics to be implemented (waiting on eml to merge)
    
    * add diag launch (also rebased onto uberfirmware)
    
    MERGE ME WITH #13!!
    
    * oh god
    
    * sometimes it causes me to tremble
    
    inside joke that no-one else will get ^---
    
    * things to make things work
    
    * yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    
    * change address
    
    * asdf
    
    * halp it work

--- rosws/src/rover_drive/include/rover_drive/hw_impl.h
+++ rosws/src/rover_drive/include/rover_drive/hw_impl.h
@@ -7,13 +7,17 @@
 
 #include <hardware_interface/robot_hw.h>
 #include <hardware_interface/joint_command_interface.h>
-#include "../../src/drive_hw/ard_device.h"
+#include <controller_diagnostics/diagnostic_interface.h>
+#include "../../../eml_uberdriver/include/eml_uberdriver/ard_device.h"
 
 namespace rover_drive {
 
+    const uint8_t ARDUINO_DRIVE_BUS = 1;
+    const uint8_t ARDUINO_DRIVE_ADDRESS = 0x20;
+
     class DriveHW {
     public:
-        DriveHW() : device(1, 0x30) {};
+        DriveHW() : device(ARDUINO_DRIVE_BUS, ARDUINO_DRIVE_ADDRESS) {};
 
         void init(hardware_interface::RobotHW *hw);
 
@@ -22,7 +26,9 @@ namespace rover_drive {
         void write();
 
     private:
-        ARDevice device;
+        void setupDeviceOnConnect();
+
+        eml_uberdriver::ARDevice device;
         union {
             double cmd[6];
             double vel[6];
@@ -32,7 +38,11 @@ namespace rover_drive {
 
         hardware_interface::JointStateInterface jnt_state_interface;
         hardware_interface::VelocityJointInterface jnt_vel_interface;
+        controller_diagnostics::DiagnosticStateInterface diag_interface;
 
+        ros::Time lastReconnectAttemptTime = ros::Time::now();
+
+        controller_diagnostics::DiagnosticHandleData diag_dhd;
     };
 }
 
