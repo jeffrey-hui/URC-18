08cad5a46d50c5855c667952bf7ecb6e3b51d4d0
mincrmatt12 <matthewmirvish@hotmail.com>
2018-04-14 16:05:51 -0400

    add comments?

--- rosws/src/rover_tennis_balls/src/tennis_ball_predictor/collider.py
+++ rosws/src/rover_tennis_balls/src/tennis_ball_predictor/collider.py
@@ -21,20 +21,27 @@ def f(pt, rays, points):
     errors = np.full([(len(rays) + len(points)) * 3], np.inf)
     for i in range(0, len(rays)):
         ray = rays[i]  # type: Ray
-        time_on_ray = ray.vec.dot((pt - ray.pos).T)
-        if time_on_ray > 0:
-            errors[i*3:(i+1)*3] = (pt - (ray.pos + time_on_ray * ray.vec)).T * ray.conf
+        time_on_ray = ray.vec.dot((pt - ray.pos).T)  # calculate t in p = ray.pos * ray.dir * t
+        if time_on_ray > 0:  # if point is on ray...
+            errors[i*3:(i+1)*3] = (pt - (ray.pos + time_on_ray * ray.vec)).T * ray.conf # do distance to nearest point along ray
         else:
-            errors[i * 3:(i + 1) * 3] = (pt - ray.pos).T * ray.conf
+            errors[i * 3:(i + 1) * 3] = (pt - ray.pos).T * ray.conf # else distance to origin of ray (farthest point back)
     k = len(rays) * 3
     for i in range(0, len(points)):
         point = points[i]
-        errors[k + i * 3:k + (i+1) * 3] = (pt - point.pos) * point.conf
+        errors[k + i * 3:k + (i+1) * 3] = (pt - point.pos) * point.conf  # for each axis of point, use distance * conf
 
-    return errors.ravel()
+    return errors.ravel()  # scipy wants it to be column-wise
 
 
 def calc_min_f(rays, points):
+    """
+    Tries to find the closest point to a bunch of rays and points
+
+    :param rays: list of rays
+    :param points: list of points
+    :return: nearest point, confidence
+    """
     rays_p = [ray.pos for ray in rays] + [point.pos for point in points]
     guess = np.stack(rays_p).mean(axis=0).ravel()
 
@@ -45,26 +52,37 @@ def calc_min_f(rays, points):
 
 
 def confidence(rays, points):
+    """
+    Calculates confidence of points and rays
+
+    :param rays: list of rays
+    :param points: list of points
+    :return: confidence 0 - 1
+    """
+
     d = 0
     div_mes = 0
     if len(rays) > 1:
         div_mes = []
-        for i in itertools.combinations(rays, 2):
-            dot = i[0].vec.dot(i[1].vec)
-            diversity = (1 - dot) * ((i[0].conf+i[1].conf) / 2)
+        for i in itertools.combinations(rays, 2):  # for all combinations of rays..
+            dot = i[0].vec.dot(i[1].vec) # take the dot product (rays are normalized so this is cos(theta))
+            diversity = abs(np.arccos(dot) / np.pi) * ((i[0].conf+i[1].conf) / 2)
+            # arccos = angle between vectors if vectors have length 1
+            # / pi scales to -1,1
+            # average confidence scales result, result is now "diversity" of angles that can be reached by these rays
             div_mes.append(diversity)
-        m = max(div_mes) * len(div_mes)
-        div_mes = sum(map(lambda x: x / float(m), div_mes))
+        m = max(div_mes) * len(div_mes) # normalize & divide by length
+        div_mes = sum(map(lambda x: x / float(m), div_mes)) # sum is now high if large diversity, but won't explode
     if len(points) > 1:
         f_p = points[0].pos
         d = 0
         for i in points[1:]:
             i = i.pos
             d += np.sqrt(
-                (f_p[0] - i[0])**2 + (f_p[1] - i[1])**2 + (f_p[2] - i[2]) ** 2
+                (f_p[0] - i[0])**2 + (f_p[1] - i[1])**2 + (f_p[2] - i[2]) ** 2  # take total distance of point n to point 0 for all n
             )
         d /= len(points)-1
         d = 1 - max(0, min(1, d))
     if len(points) == 1:
         d = 0.5
-    return (d * 0.3 + min(1, div_mes) * 0.3) + (1 - (1 / float((len(rays) + len(points)) + 1))) * 0.4
+    return (d * 0.3 + min(1, div_mes) * 0.3) + (1 - (1 / float((len(rays) + len(points)) + 1))) * 0.4 # magic!
