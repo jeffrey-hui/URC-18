7c120a98933bb9071876bf2e35d221b97f6596a9
mincrmatt12 <matthewmirvish@hotmail.com>
2018-05-02 20:51:47 -0400

    internal documentation
    AaaAaaAaaaaaAAAAAaaaAAAAA
    
     - "it does the thing that does the stuff"
     - yippee
    
     yay

--- rosws/src/rover_navigation/src/lines_layer.cpp
+++ rosws/src/rover_navigation/src/lines_layer.cpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <rover_navigation/lines_layer.h>
+#include <pluginlib/class_list_macros.h>
 
 void rover_navigation::LineLayer::raytrace(int x0, int y0, int x1, int y1, std::vector<Point> &cells) {
     // totally not stolen from another package because i couldn't be bothered to write out a line algorithm
@@ -32,3 +33,84 @@ void rover_navigation::LineLayer::raytrace(int x0, int y0, int x1, int y1, std::
     }
 }
 
+void rover_navigation::LineLayer::parseFromMsg(rover_navigation::Path &p) {
+    std::string globalFrameID = this->layered_costmap_->getGlobalFrameID();
+
+    this->waypoints.clear();
+    for (auto stampy_the_stamp : p.points) {
+        geometry_msgs::PointStamped strampy_the_cramp;
+        this->tf_->transformPoint(globalFrameID, stampy_the_stamp, strampy_the_cramp);
+        this->waypoints.push_back(strampy_the_cramp.point);
+    }
+
+    computeBounds();
+}
+
+void rover_navigation::LineLayer::computeBounds() {
+    if (this->waypoints.empty()) {
+        _minX = _minY = _maxX = _maxY = 0;
+        return;
+    }
+    _minX = _maxX = static_cast<int>(this->waypoints[0].x);
+    _minY = _maxY = static_cast<int>(this->waypoints[0].z);
+    _maxX++;
+    _maxY++;
+    
+    
+    for (int i = 1; i < this->waypoints.size(); i++) {
+        _minX = std::min(_minX, static_cast<int>(this->waypoints[i].x));
+        _maxX = std::max(_maxX, static_cast<int>(this->waypoints[i].x));
+        _minY = std::min(_minY, static_cast<int>(this->waypoints[i].y));
+        _maxY = std::max(_maxY, static_cast<int>(this->waypoints[i].y));
+    }
+}
+
+void rover_navigation::LineLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double *min_x,
+                                               double *min_y, double *max_x, double *max_y) {
+    if (!this->enabled_) return;
+    if (this->waypoints.empty()) return;
+
+    *min_x = std::min((double)_minX, *min_x);
+    *min_y = std::min((double)_minY, *min_y);
+    *max_x = std::min((double)_maxX, *max_x);
+    *max_y = std::min((double)_maxY, *max_y);
+}
+
+void rover_navigation::LineLayer::updateCosts(costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i,
+                                              int max_j) {
+    if (!enabled_) return;
+
+    std::vector<Point> points;
+    for (int i = 0; i < waypoints.size()-1; i++) {
+        raytrace(waypoints[i].x, waypoints[i].y, waypoints[i+1].x, waypoints[i+1].y, points);
+    }
+
+    for (auto e : points) {
+        unsigned int mx, my
+        if (master_grid.worldToMap(e.x, e.y, mx, my)) {
+            if (master_grid.getCost(mx, my) == costmap_2d::NO_INFORMATION) {
+                master_grid.setCost(mx, my, costmap_2d::FREE_SPACE);
+            }
+        }
+    }
+}
+
+void rover_navigation::LineLayer::deactivate() {
+    this->onInitialize();
+}
+
+void rover_navigation::LineLayer::activate() {
+    this->subscriber.shutdown();
+}
+
+void rover_navigation::LineLayer::reset() {
+    this->onInitialize();
+}
+
+void rover_navigation::LineLayer::onInitialize() {
+    ros::NodeHandle nh("~/" + name_);
+    this->subscriber = nh.subscribe("~path", 10, &LineLayer::parseFromMsg, this);
+    this->subscriber.
+}
+
+PLUGINLIB_EXPORT_CLASS(rover_navigation::LineLayer, costmap_2d::Layer)
