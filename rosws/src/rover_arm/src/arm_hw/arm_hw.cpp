230ad26e9e537eabb51db5fb5a0ee421ecf4b8e3
Matthew Mirvish <mincrmatt12@users.noreply.github.com>
2018-05-15 17:02:09 -0400

    Arm software (#14)
    SQUAAAAASH it and merge it
    
    woopeee
    
    
    * Start new arm
    
    including new stars like:
    
       - the poles
       - the box
       - the encap
    
    and returning players like:
    
       - the gripper
    
    * new ARM!!!!!
    
    * better naming
    
    * move eml_uberdriver
    
    * almost done docs for eml_uberdriver
    
    WARNING: this will probably get moved out to rtd for a seperate system, but for now
    it is staying here
    
    * fix uberfirmware
    
    * START ARM
    
    * begin work on rover arm,
    
    * more arm
    
    * stuff in progress
    
    * current version
    
    * asdf
    
    * reformat arm to a better format
    
    * add science gripper
    
    * add science controllers
    
    * reorg arm_hw
    
    * begin science arm
    
    * more arm
    
    * tune pids better
    
    * begin as
    
    * asdf - asdf
    
    * add various things
    
    * change outdoor_world to support multiple grippers
    
    a sane commit message?
    
    * further change
    
    * start work on new arm software
    
    for test
    
    it'll work
    i promise
    no i dont
    neveer mind
    
    * maybe working code for arm
    
    * changed the address to match hardware
    
    * stop spamming a you git
    
    * modify
    
    GABE WHY
    
    * yayyayayayaya
    
    why am i so tired
    
    * add motor constants
    
    * hmmmm
    
    * 2 is not wired?
    
    * HUZZAH (it works)
    
    this is a matthew commit, under gabe's account(maybe)
    
    * last minute changes

---
+++ rosws/src/rover_arm/src/arm_hw/arm_hw.cpp
@@ -0,0 +1,145 @@
+//
+// Created by matthew on 2/15/18.
+//
+
+#include <rover_arm/arm_hw/arm_hw.h>
+#include <boost/algorithm/string.hpp>
+#include "motor_params.h"
+
+namespace rover_arm {
+    uint16_t convertEffToMotorV(double value) {
+        double effort_normalized = value / MOTOR_EXERTED_EFFORT;
+        double offset = MOTOR_OFFSET * effort_normalized;
+
+        // todo: update for different joints
+
+        return (uint16_t)(MOTOR_MID + offset);
+    }
+
+    double convertToPositionOffsetRotate(int ticks) {
+        return (ticks / TICKS_PER_REVOLUTION) * (3.1415926 * 2);
+    }
+}
+
+void rover_arm::ArmHW::init(hardware_interface::RobotHW *hw) {
+    ros::NodeHandle nh("/");
+
+    if (!this->device.isDisconnected()) {
+        this->setupDeviceOnConnect();
+    }
+
+    this->lastReconnectAttemptTime = ros::Time::now();
+
+    hardware_interface::ActuatorStateHandle jsh_io("arm_xy_inner_motor", &pos[0], &eff[0], &vel[0]);
+    hardware_interface::ActuatorStateHandle jsh_su("arm_xy_outer_motor", &pos[1], &eff[1], &vel[1]);
+    hardware_interface::ActuatorStateHandle jsh_sp("arm_slide_motor", &pos[2], &eff[2], &vel[2]);
+    hardware_interface::ActuatorStateHandle jsh_tl("arm_spin_inner_motor", &pos[3], &eff[3], &vel[3]);
+
+    this->act_state_interface.registerHandle(jsh_io);
+    this->act_state_interface.registerHandle(jsh_su);
+    this->act_state_interface.registerHandle(jsh_sp);
+    this->act_state_interface.registerHandle(jsh_tl);
+
+    hardware_interface::ActuatorHandle jh_io(jsh_io, &cmd[0]);
+    hardware_interface::ActuatorHandle jh_su(jsh_su, &cmd[1]);
+    hardware_interface::ActuatorHandle jh_sp(jsh_sp, &cmd[2]);
+    hardware_interface::ActuatorHandle jh_tl(jsh_tl, &cmd[3]);
+
+    this->act_eff_interface.registerHandle(jh_io);
+    this->act_eff_interface.registerHandle(jh_su);
+    this->act_eff_interface.registerHandle(jh_sp);
+    this->act_eff_interface.registerHandle(jh_tl);
+
+    diag_dhd.hardwareID = "due-arm";
+    diag_dhd.data["connected"] = !this->device.isDisconnected() ? "yes" : "no";
+    diag_dhd.data["device"] = "/dev/i2c-" + std::to_string(ARDUINO_ARM_BUS);
+    diag_dhd.data["address"] = std::to_string(ARDUINO_ARM_ADDRESS);
+    controller_diagnostics::DiagnosticHandle dH("arm_arduino", &diag_dhd);
+
+    diag_interface.registerHandle(dH);
+
+    hw->registerInterface(&act_eff_interface);
+    hw->registerInterface(&act_state_interface);
+    hw->registerInterface(&diag_interface);
+
+    std::string robot_description;
+    if (!nh.getParam("robot_description", robot_description)) {
+        ROS_FATAL_STREAM("Failed to load robot_description, this node will probably now crash");
+    }
+
+    this->transmission_loader_ = new transmission_interface::TransmissionInterfaceLoader(hw, &this->robot_transmissions);
+    transmission_interface::TransmissionParser parser;
+    std::vector<transmission_interface::TransmissionInfo> infos;
+    parser.parse(robot_description, infos);
+    for (auto e : infos) {
+        if (boost::starts_with(e.name_, "arm")) {
+            if (!this->transmission_loader_->load(e)) {
+                ROS_FATAL_STREAM("ASDF");
+            }
+        }
+    }
+
+}
+
+void rover_arm::ArmHW::write() {
+    this->robot_transmissions.get<transmission_interface::JointToActuatorEffortInterface>()->propagate();
+    //ROS_INFO_STREAM("a " << cmd[1]);
+    if (!this->device.isDisconnected()) {
+	//ROS_INFO_STREAM("ASDFADSFADSFADSFADS");
+        this->device.writeMicroseconds(MOTOR_INNEROUTR, convertEffToMotorV(cmd[0]));
+        this->device.writeMicroseconds(MOTOR_SLIDEUNIT, convertEffToMotorV(cmd[1]));
+        this->device.writeMicroseconds(MOTOR_SLIDEPOLE, convertEffToMotorV(cmd[2]));
+        this->device.writeMicroseconds(MOTOR_GRIPPTILT, convertEffToMotorV(cmd[3]));
+    }
+}
+
+void rover_arm::ArmHW::read() {
+    this->robot_transmissions.get<transmission_interface::ActuatorToJointStateInterface>()->propagate();
+    if (this->device.isDisconnected()) {
+        diag_dhd.data["connected"] = "no";
+        diag_dhd.status = diagnostic_msgs::DiagnosticStatus::ERROR;
+        diag_dhd.message = "The arduino is not connected or is not listening to i2c";
+
+        if ((ros::Time::now() - this->lastReconnectAttemptTime).toSec() > 3.0) {
+            this->lastReconnectAttemptTime = ros::Time::now();
+            if (this->device.tryOpen()) {
+                this->setupDeviceOnConnect();
+                ROS_INFO_STREAM("Connected!");
+            }
+            else {
+                ROS_WARN_STREAM("Failed to connect to I2C device, retry in 3 seconds");
+            }
+        }
+    }
+    else {
+        diag_dhd.data["connected"] = "yes";
+        diag_dhd.status = diagnostic_msgs::DiagnosticStatus::OK;
+        diag_dhd.message = "The arduino is connected and responding to i2c";
+	
+        int ticks_inneroutr = this->jointEncoders[0].encoderValue();
+        int ticks_slideunit = this->jointEncoders[1].encoderValue();
+        int ticks_slidepole = this->jointEncoders[2].encoderValue();
+        int ticks_gripptilt = this->jointEncoders[3].encoderValue();
+
+        pos[1] = convertToPositionOffsetRotate(ticks_inneroutr); // todo: get gear crappo for this
+        pos[0] = convertToPositionOffsetRotate(ticks_slideunit);
+        pos[2] = 0; // todo
+        pos[3] = convertToPositionOffsetRotate(ticks_gripptilt);
+	//ROS_INFO_STREAM("abc " << ticks_inneroutr << " " << ticks_slideunit << " " << ticks_gripptilt);
+    }
+}
+
+void rover_arm::ArmHW::setupDeviceOnConnect() {
+    this->device.openPinAsMotor(MOTOR_INNEROUTR);
+    this->device.openPinAsMotor(MOTOR_SLIDEUNIT);
+    this->device.openPinAsMotor(MOTOR_SLIDEPOLE);
+    this->device.openPinAsMotor(MOTOR_GRIPPTILT);
+    //ROS_INFO_STREAM("ASDF");
+    this->jointEncoders[0] = this->device.openPinAsEncoder(ENCODER_INNEROUTR_A, ENCODER_INNEROUTR_B);
+    //ROS_INFO_STREAM(this->device.isDisconnected());
+    this->jointEncoders[1] = this->device.openPinAsEncoder(ENCODER_SLIDEUNIT_A, ENCODER_SLIDEUNIT_B);
+    this->jointEncoders[2] = this->device.openPinAsEncoder(ENCODER_SLIDEPOLE_A, ENCODER_SLIDEPOLE_B);
+    this->jointEncoders[3] = this->device.openPinAsEncoder(ENCODER_GRIPPTILT_A, ENCODER_GRIPPTILT_B);
+    //ROS_INFO_STREAM("ASDF" << this->device.isDisconnected());
+}
+
---
+++ rosws/src/rover_arm/src/arm_science_node/teleop.py
@@ -0,0 +1,103 @@
+import rospy
+import threading
+from sensor_msgs.msg import Joy, JointState
+from std_msgs.msg import Float64, Float64MultiArray
+import cont_mode
+
+DRILL_POWER = 0.25
+Y_SPEED = 0.25 / 30.0
+THETA_SPEED = 1.57 / 30.0
+
+
+class Teleop(threading.Thread):
+    instance = None  # type: Teleop
+
+    def __init__(self):
+        super(Teleop, self).__init__()
+
+        Teleop.instance = self
+        self.joy_sub = rospy.Subscriber("/joy", Joy, queue_size=10, callback=self.on_joy)
+        self.joint_state_sub = rospy.Subscriber("/joint_states", JointState, queue_size=2, callback=self.on_joint_state)
+        self.position = None
+        self.needs_stop = False
+        self.drill_on = False
+        self.teleop_on = False
+        self.lock_theta = False
+        self.teleop_done = threading.Condition()
+        self.cooldown = 0
+
+        self.last_joy_state = None  # type: Joy
+        self.joy_state = None
+        self.drill_pub = rospy.Publisher("/drill_controller/command", Float64, queue_size=10)
+        self.target_pub = rospy.Publisher("/arm_manual_controller/command", Float64MultiArray, queue_size=10)
+        self.rate = rospy.Rate(30)
+
+        self.target_position = None
+
+    def on_joint_state(self, m):
+        self.position = [
+            max(0, m.position[m.name.index("arm_slide_pole_to_arm_slider_unit")]),
+            m.position[m.name.index("arm_slider_unit_to_arm_xy_pole")],
+            max(0, m.position[m.name.index("science_crossbar_to_science_sensors")])
+        ]
+
+    def on_joy(self, m):
+        self.last_joy_state = self.joy_state
+        self.joy_state = m
+
+    def enable_drill(self):
+        self.drill_on = True
+
+    def enable_teleop(self):
+        cont_mode.set_manual_mode_on_arm()
+        self.teleop_on = True
+        while self.position is None:
+            pass
+        self.target_position = self.position[:]
+        self.last_joy_state = None
+
+    def lock_theta(self):
+        self.lock_theta = False
+
+    def disable_teleop(self):
+        self.drill_on = False
+        self.teleop_on = False
+        self.lock_theta = False
+        self.last_joy_state = None
+        self.drill_pub.publish(0)
+
+    def wait_for_teleop_done(self):
+        with self.teleop_done:
+            self.teleop_done.wait()
+
+    def stop(self):
+        self.needs_stop = True
+
+    def falling_edge(self, i):
+        if self.last_joy_state is None:
+            return False
+        if self.last_joy_state.buttons[i] and not self.joy_state.buttons[i] and self.cooldown == 0:
+            self.cooldown = 15
+            return True
+        return False
+
+    def run(self):
+        while not self.needs_stop:
+            self.rate.sleep()
+            if self.joy_state is not None:
+                if self.drill_on:
+                    self.drill_pub.publish(DRILL_POWER * int(self.joy_state.buttons[0]))
+                if self.teleop_on:
+                    self.cooldown = max(0, self.cooldown - 1)
+                    y_offset = self.joy_state.axes[1] * Y_SPEED
+                    y_s_offset = self.joy_state.axes[5] * Y_SPEED
+                    theta_offset = self.joy_state.axes[2] * THETA_SPEED
+                    self.target_position[0] -= y_offset
+                    if not self.lock_theta:
+                        self.target_position[1] += theta_offset
+                    self.target_position[2] += y_s_offset
+                    self.target_pub.publish(Float64MultiArray(data=self.target_position))
+                    if self.falling_edge(1):
+                        self.disable_teleop()
+                        with self.teleop_done:
+                            self.teleop_done.notify()
