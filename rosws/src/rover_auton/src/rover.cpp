a9be4ae37913473c4fcb61129c220a6b26e583df
hamzaMahdi <hamzamahdi96@gmail.com>
2018-05-15 17:38:17 -0400

    added GPS and Mag input

--- rosws/src/rover_auton/src/rover.cpp
+++ rosws/src/rover_auton/src/rover.cpp
@@ -3,26 +3,36 @@
 //
 
 #include "rover.h"
-std::string receiveGPS;
 ros::Time last_time, current_time;
 control_toolbox::Pid LinPID;
 control_toolbox::Pid AngPID;
+sensor_msgs::NavSatFix GPSData;
+sensor_msgs::MagneticField magData;
 rover::rover(){
 }
 rover::~rover(){}
 double rover::getAngCoordinates(){
-    return 1;
-
+    double angle  = atan2(GPSData.longitude,GPSData.latitude);
+    return angle;
 }
 double rover::getLinCoordinates(){
-    return 1;
+    return sqrt((GPSData.longitude)*(GPSData.longitude)+(GPSData.latitude)*(GPSData.latitude));
 
 }
+
+void rover::calculateTargetAng(){
+    targetAng = atan2(targetLong,targetLat);
+}
+void rover::calculateTargetLin(){
+    targetAng = sqrt(targetLat*targetLat+targetLong*targetLong);
+}
 double rover::calculateDrivePower(){
+    calculateTargetLin();
     double errorLin  = targetLin - rover::getLinCoordinates();
     return LinPID.computeCommand(errorLin,current_time-last_time);
 }
 double rover::calculateTurnPower(){
+    calculateTargetAng();
     double errorAng  = targetAng - rover::getAngCoordinates();
     return AngPID.computeCommand(errorAng,current_time-last_time);
 }
