634b8941d08ed9767148b6565af37402d4089ab9
mincrmatt12 <matthewmirvish@hotmail.com>
2018-05-15 20:41:21 -0400

    start imugps

---
+++ rosws/src/rover_imugps/src/gps_forwarder.cpp
@@ -0,0 +1,12 @@
+//
+// Created by matthew on 5/15/18.
+//
+
+#include <ros/ros.h>
+
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "gps_forwarder");
+
+    // this has to take simpli2c and output nmea_msgs/Sentence messages.
+    return 0;
+}
