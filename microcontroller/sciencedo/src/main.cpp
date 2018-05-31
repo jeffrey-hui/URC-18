20d461196446e5a791b0e204910f659ce25ce7ec
mincrmatt12 <matthewmirvish@hotmail.com>
2018-05-31 07:52:37 -0600

    fjeff

--- microcontroller/sciencedo/src/main.cpp
+++ microcontroller/sciencedo/src/main.cpp
@@ -1,6 +1,8 @@
-
+#include <Arduino.h>
 #include <SoftwareSerial.h>             //we have to include the SoftwareSerial library, or else we can't use it
 #include <Servo.h>                      //TODO ADD DEPENDENCIES
+#include <Wire.h>
+
 #define rx 2                            //define what pin rx is going to be
 #define tx 3                            //define what pin tx is going to be
 #define ADD_BASE 0x40
@@ -14,19 +16,17 @@
 //have we received all the data from the Atlas Scientific product
 SoftwareSerial myserial(rx, tx);        
 String inputstring = "";                
-String sensorstring = "";               
-boolean input_string_complete = false;  
-boolean sensor_string_complete = false; 
+String sensorstring = "";
+
+Servo fjeff;
+bool input_string_complete = false;
+bool sensor_string_complete = false;
 float DO;                               
+int ADDRESS = 0;
 
 //if the hardware serial port_0 receives a char
 //read the string until we see a <CR>
 //set the flag used to tell if we have received a completed string from the PC
-void serialEvent()
-{                                           
-  inputstring = Serial.readStringUntil(13); 
-  input_string_complete = true;             
-}
 void set_address(){
     pinMode(ADD_PIN_1, INPUT);
     pinMode(ADD_PIN_2, INPUT);
@@ -34,22 +34,25 @@ void set_address(){
     ADDRESS += digitalRead(ADD_PIN_1);
     ADDRESS += digitalRead(ADD_PIN_2) * 2;
 }
-void writeServo(Servo s){
-  while(1 < Wire.available){
-    s.write(Wire.read());
-    delay(1000);
+
+void handleGet(int bytes) {
+  char req[bytes];
+  Wire.readBytes(req, bytes);
+  if (req[0] == 0x01) {
+    Wire.write(reinterpret_cast<char *>(&DO), sizeof(float));
+  }
+  else {
+    short motorCommand = *(reinterpret_cast<short *>(req + 1));
+    fjeff.writeMicroseconds(motorCommand);
   }
 }
 
 void setup()
-{                          
-  Servo SERVO;
-  SERVO.attach(9);
+{
   Wire.begin(ADDRESS);
-  Wire.onRecieve(writeServo(SERVO));
-  pinMode(SERVO,output);        
-  Serial.begin(9600);       
-  myserial.begin(9600);     
+  Wire.onReceive(handleGet);
+  fjeff.attach(SERVO);
+  myserial.begin(9600);
   inputstring.reserve(10);  
   sensorstring.reserve(30); 
 }
@@ -76,18 +79,17 @@ void loop()
   }
   if (sensor_string_complete == true)
   {                               //if a string from the Atlas Scientific product has been received in its entirety
-    Serial.println(sensorstring); //send that string to the PC's serial monitor
-    /*                                                //uncomment this section to see how to convert the DO reading from a string to a float 
+                                                    //uncomment this section to see how to convert the DO reading from a string to a float
     if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
       DO = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
       if (DO >= 6.0) {                                //if the DO is greater than or equal to 6.0
-        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+
       }
       if (DO <= 5.99) {                               //if the DO is less than or equal to 5.99
-        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+
       }
     }
-    */
+
     sensorstring = "";              //clear the string
     sensor_string_complete = false; //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
   }
