4b17bfae492b75e606e6cc3c71fe7015878ce61e
hamzaMahdi <hamzamahdi96@gmail.com>
2018-06-02 05:43:18 -0400

    fixed shit

--- rosws/src/dank_auton/src/jank.cpp
+++ rosws/src/dank_auton/src/jank.cpp
@@ -6,29 +6,30 @@
 
 #include<ros/ros.h>
 #include <geometry_msgs/Twist.h>
-
+#include <std_msgs/Float64.h>
 
 int main(int argc, char** argv){
     ros::init(argc,argv,"rover");
     ros::NodeHandle nh;
-    ros::Publisher rover_pub = nh.advertise<geometry_msgs::Twist>("/wheel_diff_drive_controller/cmd_vel", 100);
-    ros::Rate loop_rate(10);
-    geometry_msgs::Twist pwr_val;
+    ros::Publisher rover_pub = nh.advertise<std_msgs::Float64>("/left_wheels_controller/cmd", 100);
+    ros::Publisher rover_pub1 = nh.advertise<std_msgs::Float64>("/right_wheels_controller/cmd", 100);
 
-    pwr_val.linear.x = 1;
-    pwr_val.angular.z = 0;
+    ros::Rate loop_rate(10);
+   std_msgs::Float64 pwr_val;
+
+    pwr_val.data = 1;
     int counter = 0;
     while(ros::ok()) {
+        pwr_val.data = 1;
         rover_pub.publish(pwr_val);
-        pwr_val.linear.x = 1;
-        rover_pub.publish(pwr_val);
+        rover_pub1.publish(pwr_val);
         if (counter > 50)
             break;
         counter++;
         ros::spinOnce();
         loop_rate.sleep();
     }
-    pwr_val.linear.x = 0;
+    pwr_val.data = 0;
     rover_pub.publish(pwr_val);
     ros::spinOnce();
     ROS_INFO("Reached Target - VEX>>>>URC");
